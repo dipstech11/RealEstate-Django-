d917861e2f429c997f675995e793d68484866073
dipsdeepak11 <deepak.yadav@xoriant.com>
2019-05-13 21:30:55 +0530

    admin UI changes

--- realstatewebsite/listings/admin.py
+++ realstatewebsite/listings/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Listing
 
-# Register your models here.
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_publised', 'price', 'list_date', 'realtor')
+    list_display_links = ('id','title','realtor')
+    list_filter = ('realtor',)
+    list_editable = ('is_publised',)
+    search_fields = ('title','realtor','price','address', 'city', 'state', 'zipcode')
+    list_per_page = 25
 
-admin.site.register(Listing)
+
+admin.site.register(Listing, ListingAdmin)
