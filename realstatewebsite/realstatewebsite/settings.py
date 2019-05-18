63ff476769a735f13b29217bf9c98ee8eedb3e59
dipstech11 <49149937+dipstech11@users.noreply.github.com>
2019-05-18 18:16:40 +0530

    Update settings.py

--- realstatewebsite/realstatewebsite/settings.py
+++ realstatewebsite/realstatewebsite/settings.py
@@ -150,6 +150,5 @@ MESSAGE_TAGS = {
 # EMAIL settings
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "dipstech2020@gmail.com"
-EMAIL_HOST_PASSWORD = "DPKydv@1234"
+#email hidden for security reason
 EMAIL_USE_TLS = True
